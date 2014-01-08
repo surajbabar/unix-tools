75516b6e690fcb202bf859962287600d19853dd9
suraj <surajbab@thoughtworks.com>
2014-01-08 23:11:35 +0530

    implemented WC library  which performs wc operations on file which consumer gives

---
+++ src/surajbab/unixTools/wc/WC.java
@@ -0,0 +1,23 @@
+package surajbab.unixTools.wc;
+
+import surajbab.unixTools.Text;
+import surajbab.unixTools.lib.File;
+
+
+public class WC {
+	public static void main(String[] args) {
+    String filename =args[0];
+	String fileContent;
+
+    File file = new File();
+    Text text = new Text();
+
+    fileContent = file.readFile(filename);
+
+    int lines =text.lines(fileContent);
+    int words =text.words(fileContent);
+	int chars =text.chars(fileContent);
+
+	System.out.print("   "+lines+"\t"+words+"\t"+chars+"\t"+filename);
+	}
+}
