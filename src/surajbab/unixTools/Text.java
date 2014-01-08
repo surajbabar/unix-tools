8bcb22c5aa925690ce48c5c7a501fa990939168a
suraj <surajbab@thoughtworks.com>
2014-01-08 23:06:51 +0530

    implemented Text class which performs wc operations on string

---
+++ src/surajbab/unixTools/Text.java
@@ -0,0 +1,15 @@
+package surajbab.unixTools;
+import java.lang.String;
+public class Text {
+    public int lines(String text){
+        String  lines[] =text.split("\r\n");
+        return lines.length;
+    }
+    public int words(String text){
+        String  words[] =text.split(" ");
+        return words.length+lines(text)-1;
+    }
+    public int chars(String text){
+        return text.length();
+    }
+}
