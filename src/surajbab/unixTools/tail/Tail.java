6732683d9937906abb4dca37265eb5a7d6fd7dac
suraj <surajbab@thoughtworks.com>
2014-01-10 13:55:15 +0530

    Tail completed with count and without option

--- src/surajbab/unixTools/tail/Tail.java
+++ src/surajbab/unixTools/tail/Tail.java
@@ -4,20 +4,20 @@ import surajbab.unixTools.lib.File;
 
 public class Tail {
     public static void main(String[] args) {
-        int count=10;
+        int count=10,noOfLines;
         String line,tailLines = "",filename = args[0],fileContent = "";
         if(args[0].startsWith("-")) {
             count = Integer.parseInt(args[0].substring(1));
             filename = args[1];
         }
-        fileContent = File.readFile(filename);
+        fileContent = new File().readFile(filename);
         String lines[] = fileContent.split("\r\n");
-        int noOfLines = lines.length - count;
+        noOfLines = lines.length - count;
+        if(noOfLines<0) noOfLines =0;
         for (int i = noOfLines; i < lines.length; i++) {
             tailLines += lines[i]+"\n";
         }
         System.out.println(tailLines);
-       ;
     }
 
 }
