95fffb788af807c7cba459314ac282e7c0637622
suraj <surajbab@thoughtworks.com>
2014-01-11 12:04:13 +0530

    cut client

---
+++ src/Client/CutClient.java
@@ -0,0 +1,15 @@
+package Client;
+
+import surajbab.unixTools.cut.Cut;
+import surajbab.unixTools.lib.File;
+
+public class CutClient {
+    public static void main(String[] args) {
+    Cut cutter = new Cut();
+    int fieldValue = Integer.parseInt(args[1]);
+    String delimitor = args[2];
+    String fileData = new File().readFile(args[0]);
+    String columnData = cutter.getFields(fileData,fieldValue,delimitor);
+    System.out.println(columnData);
+    }
+}
