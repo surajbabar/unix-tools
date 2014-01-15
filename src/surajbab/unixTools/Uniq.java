528aa3adf1a0f9c959a5d718886baced694edd96
suraj <surajbab@thoughtworks.com>
2014-01-15 10:41:10 +0530

    handeled LF and CRLF issue

--- src/surajbab/unixTools/Uniq.java
+++ src/surajbab/unixTools/Uniq.java
@@ -2,7 +2,7 @@ package surajbab.unixTools;
 
 public class Uniq {
     public String UniqLines(String fileData) {
-        String lines[] = fileData.split("\n");
+        String lines[] = fileData.split("\r\n");
         String uniqData = lines[0] + "\r\n";
         for (int line = 1; line < lines.length; line++) {
             if (lines[line - 1].equals(lines[line]))
