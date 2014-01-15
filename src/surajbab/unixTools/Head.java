1a3e3992ad8de1245a42a4a50f6e72a8317e28fa
suraj <surajbab@thoughtworks.com>
2014-01-15 10:22:11 +0530

    handeled empty File

--- src/surajbab/unixTools/Head.java
+++ src/surajbab/unixTools/Head.java
@@ -1,12 +1,13 @@
 package surajbab.unixTools;
 
 public class Head {
-    public String headContent(String fileContent , int count){
-    String lines[] =fileContent.split("\r\n");
-    String result="";
-        for (int line = 0; line < count-1; line++)
-                result+=lines[line]+"\r\n";
-        result+=lines[count-1];
-       return result;
+    public String headContent(String fileContent, int count) {
+        String lines[] = fileContent.split("\r\n");
+        String result = "";
+        if(lines.length<count) return fileContent;
+        for (int line = 0; line < count - 1; line++)
+            result += lines[line] + "\r\n";
+        result += lines[count - 1];
+        return result;
     }
 }
