99bab58cc22105faf5d727af0da26511c7ea254c
suraj <surajbab@thoughtworks.com>
2014-01-20 10:29:31 +0530

    separated method to src directory

--- src/Client/SpaceReducerClient.java
+++ src/Client/SpaceReducerClient.java
@@ -1,11 +1,13 @@
 package Client;
 
+import surajbab.unixTools.SpaceReducer;
 import surajbab.unixTools.lib.File;
 
 public class SpaceReducerClient {
     public static void main(String[] args) {
+        SpaceReducer sp = new SpaceReducer();
         String fileData;
         fileData = new File().readFile(args[0]);
-        System.out.println(fileData);
+        System.out.println(sp.reduceSpaces(fileData));
     }
 }
