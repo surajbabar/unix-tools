98ea6d205fa9f44dbfe5995d33969c005aea2137
suraj <surajbab@thoughtworks.com>
2014-01-22 10:37:22 +0530

    sort functionality works

--- src/surajbab/unixTools/Sort.java
+++ src/surajbab/unixTools/Sort.java
@@ -1,8 +1,23 @@
 package surajbab.unixTools;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static com.sun.javafx.fxml.expression.Expression.split;
 
 public class Sort {
+    public String sort(String content, char reverse) {
+        String Data[];
+        String sortedData = "";
+        Data = content.split("\r\n");
+        Arrays.sort(Data);
+        for (int i =Data.length-1;i>=0;i--)
+            sortedData = sortedData.concat(Data[i]).concat("\r\n");
+        return sortedData;
+    }
+
     public String sort(String content) {
         String Data[];
         String sortedData = "";
--- tests/surajbab/unixTools/SortTest.java
+++ tests/surajbab/unixTools/SortTest.java
@@ -2,12 +2,30 @@ package surajbab.unixTools;
 
 import org.junit.Test;
 
-/**
- * Created by surajbab on 1/20/14.
- */
+import static junit.framework.Assert.assertEquals;
+
 public class SortTest {
     @Test
-    public void testSort() throws Exception {
+    public void testSortDataInAscendingOrder() throws Exception {
+        String data = "b\r\nc\r\na";
+        String expected = "a\r\nb\r\nc\r\n";
+        Sort sort = new Sort();
+
+        String actual = sort.sort(data);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSortDataInDescendingOrder() throws Exception {
+        String data = "b\r\nc\r\na";
+        String expected = "c\r\nb\r\na\r\n";
+        Sort sort = new Sort();
+
+        String actual = sort.sort(data, 'r');
+
+        assertEquals(expected, actual);
 
     }
+
 }
